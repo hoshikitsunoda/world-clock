d45a92508234ed9caaa83d8bb7d9612a712c948a
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-23 13:46:55 -0700

    Create a function to create elements. #1

--- Public/main.js
+++ Public/main.js
@@ -5,4 +5,19 @@ const timeZones = (zones) => zones.map(zone => (
   }
 ))
 
-console.log(timeZones)
+const $time = document.createElement('div')
+const $value = document.createElement('div')
+
+$time.setAttribute('class', 'time')
+
+const setTime = (time) => {
+  $value.setAttribute('class', 'value')
+  $value.textContent = `
+    ${time.zone}
+    ${time.time}
+  `
+  return $value
+}
+
+$time.appendChild(setTime($value))
+document.body.appendChild($time)
