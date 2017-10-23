383d5e62f6420b3949d91d5323b6c3cb08ec6504
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-23 11:48:55 -0700

    Set up app.get with timezone path. #1

--- seed.js
+++ seed.js
@@ -1,33 +1,23 @@
 const { MongoClient } = require('mongodb')
 const url = 'mongodb://localhost/worldclock'
 
-MongoClient.connect(url, (error, db) => {
+MongoClient.connect(url, (err, db) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
   const timezones = db.collection('timezones')
 
-  timezones
-    .fint({})
-    .toArray()
-    .then(results => {
       timezones
-        .insertMany([
-          'Asia/Tokyo',
-          'Asia/Dhaka',
-          'Asia/Dubai',
-          'Europe/Moscow',
-          'Europe/Rome',
-          'Europe/London',
-          'America/Sao_Paulo'
-          'America/New_York',
-          'America/Los_Angeles'
-        ]).map(zone => {
-          zone: zone
-        })
-        .then(() => db.close())
-        .catch(err => console.error(err))
-    })
-    .catch(err => console.error(err))
-})
+      .insertMany([
+        { timezone: 'Asia/Tokyo' },
+        { timezone: 'Asia/Dhaka' },
+        { timezone: 'Asia/Dubai' },
+        { timezone: 'Europe/Moscow' },
+        { timezone: 'Europe/Rome' },
+        { timezone: 'Europe/London' },
+        { timezone: 'America/Sao_Paulo' },
+        { timezone: 'America/New_York' },
+        { timezone: 'America/Los_Angeles' },
+      ])
+  })
